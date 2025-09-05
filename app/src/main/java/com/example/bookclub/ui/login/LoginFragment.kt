48c57bc8ee06189e4e699179b6623506cc44034b
marcopetcu <petcugeorge24@gmail.com>
2025-09-06 01:54:10 +0300

    initial commit

---
+++ app/src/main/java/com/example/bookclub/ui/login/LoginFragment.kt
@@ -0,0 +1,43 @@
+package com.example.bookclub.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.bookclub.R
+import android.widget.TextView
+
+class LoginFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?  = inflater.inflate(R.layout.fragment_login, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.tv_register).setOnClickListener {
+            goToRegister()
+        }
+
+        view.findViewById<Button>(R.id.btn_login).setOnClickListener {
+            goToHome()
+        }
+    }
+
+    private fun goToRegister() {
+        val action = LoginFragmentDirections.actionLoginFragmentToRegisterFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToHome() {
+        val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment()
+        findNavController().navigate(action)
+    }
+
+}
---
+++ app/src/main/java/com/example/bookclub/ui/register/RegisterFragment.kt
@@ -0,0 +1,18 @@
+package com.example.bookclub.ui.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bookclub.R
+
+class RegisterFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View?  = inflater.inflate(R.layout.fragment_register, container, false)
+
+}
